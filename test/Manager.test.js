f1f91f360eb4b01e95f04c0d18a5e43cfda20b82
Pavel Darii <paveldarii@yahoo.com>
2020-12-19 19:30:23 -0500

    added test to the GetRole and gerOfficeNumber functions

---
+++ test/Manager.test.js
@@ -0,0 +1,15 @@
+const Manager = require("../lib/manager");
+
+describe("Manager class", () => {
+  it("getGithub should return managers's office number", () => {
+    const manager = new Manager("Pavel", 1, "pd@yahoo.com", 2);
+
+    expect(manager.getOfficeNumber()).toBe(2);
+  });
+
+  it("getName should return Engineer", () => {
+    const manager = new Manager("Pavel", 1, "pd@yahoo.com", "pd");
+
+    expect(manager.getRole()).toEqual("Manager");
+  });
+});
