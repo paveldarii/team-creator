d7a5389dc6994bb5357f7c086bfe37c7698aa40c
Pavel Darii <paveldarii@yahoo.com>
2020-12-19 20:27:49 -0500

    created test to check wheter the id increase or not with the creation of each object

--- test/Employee.test.js
+++ test/Employee.test.js
@@ -2,25 +2,25 @@ const Employee = require("../lib/employee");
 
 describe("Employee class", () => {
   it("getName should return the name of the employee", () => {
-    const employee = new Employee("Pavel", 1, "pd@yahoo.com");
+    const employee = new Employee("Pavel", "pd@yahoo.com");
 
     expect(employee.getName()).toBe("Pavel");
   });
 
-  it("getId should return the id of the employee", () => {
-    const employee = new Employee("Pavel", 1, "pd@yahoo.com");
-
-    expect(employee.getId()).toEqual(1);
-  });
-
   it("getEmployee should return the email of the employee", () => {
-    const employee = new Employee("Pavel", 1, "pd@yahoo.com");
+    const employee = new Employee("Pavel", "pd@yahoo.com");
 
     expect(employee.getEmail()).toBe("pd@yahoo.com");
   });
+  it("getId should return a value for the object id different from previous object id", () => {
+    const employeeOne = new Employee("Pavel", "pd@yahoo.com");
+    const employeeTwo = new Employee("Mike", "mk@yahoo.com");
+
+    expect(employeeOne.getId()).toBeLessThan(employeeTwo.getId());
+  });
 
   it("getRole should return Employee", () => {
-    const employee = new Employee("Pavel", 1, "pd@yahoo.com");
+    const employee = new Employee("Pavel", "pd@yahoo.com");
     expect(employee.getRole()).toEqual("Employee");
   });
 });
